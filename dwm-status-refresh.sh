7cd4bf52f1e745b6ab688c5b29e5c72b20b8a1b4
chenyc <chen.yaocan@infoloop.cn>
2020-09-23 16:12:51 +0800

    update status bar

--- dwm-status-refresh.sh
+++ dwm-status-refresh.sh
@@ -2,12 +2,12 @@
 
 print_privoxy(){
     PIDS=`ps -ef | grep privoxy | grep -v grep | grep -v set-privoxy | awk '{print $2}'`;
-    if [ "$PIDS" != "" ]; then echo "privoxy : on |"; fi
+    if [ "$PIDS" != "" ]; then echo "👓|"; fi
 }
 
 print_cpu(){
-    cpuusage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}');
-    echo "$cpuusage";
+    cpuusage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}');
+    echo "👽$cpuusage";
 }
 
 print_mem(){
@@ -15,21 +15,34 @@ print_mem(){
 	memtotal=$(($(grep -m1 'MemTotal:' /proc/meminfo | awk '{print $2}') / 1000000));
     memavailablepercent=$(expr $memavailable / $memtotal);
     memusedpercent=$(expr 100 - $memavailablepercent);
-	echo "$memusedpercent%";
+	echo "$memusedpercent";
 }
 
 print_alsa(){
-    VOL=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/");
-    echo "$VOL%"
+    vol=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/");
+    volstatus=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)\]$/\1/");
+    if [ "$vol" -eq 0 ] || [ "$volstatus" == "off" ]; then vol="--"; volsign="🔇";
+    elif [ "$vol" -gt 0 ] && [ "$vol" -le 33 ]; then volsign="🔈";
+    elif [ "$vol" -gt 33 ] && [ "$vol" -le 66 ]; then volsign="🔉";
+    else volsign="🔊"; fi
+    echo "$volsign$vol"
 }
 
 print_bat(){
-	echo "$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc))%";
+    batpercent=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc))
+    if [ "$(acpi -b | grep discharging)" == "" ]; then chargesign=""; fi
+    if [ "$batpercent" -le 10 ]; then batsign="";
+    elif [ "$batpercent" -le 25 ]; then batsign="";
+    elif [ "$batpercent" -le 50 ]; then batsign="";
+    elif [ "$batpercent" -le 95 ]; then batsign="";
+    else chargesign=""; batsign="☻"; fi
+    echo "$chargestatus$batsign$batpercent"
 }
 
 print_date(){
-	date '+%m/%d %H:%M'
+    date=$(date '+%m月%d日 %H点%M分')
+    echo "$date"
 }
 
-xsetroot -name "$(print_privoxy) $(print_cpu) | $(print_mem) | $(print_alsa) | $(print_bat) | $(print_date)"
+xsetroot -name "$(print_privoxy)$(print_date)|$(print_cpu)%|$(print_mem)%|$(print_alsa)%|$(print_bat)"
 exit 0
