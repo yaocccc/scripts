61d3735acc1e43f5b6b343ee8cb84cd21cc0e346
yaocccc <chen.yaocan@infoloop.cn>
2020-09-24 15:44:32 +0800

    add qq daemon to autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -7,6 +7,7 @@ nm-applet &
 electron-ssr &
 xfce4-power-manager &
 bluetoothctl power on &
+/usr/lib/gsd-xsettings &
 ~/scripts/set-screen.sh work &
 ~/scripts/set-privoxy.sh on &
 ~/scripts/autostart_wait.sh &
