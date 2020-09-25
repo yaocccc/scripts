f93ff308bd53e7f20ba958b991240071ddb66b7c
yaocccc <chen.yaocan@infoloop.cn>
2020-09-25 13:32:07 +0800

    remove autostart privoxy

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /bin/bash ~/scripts/dwm-status.sh &
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -o 0.95 -i 0.90 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 xautolock -time 10 -locker blurlock &
 nm-applet &
 electron-ssr &
@@ -9,5 +9,4 @@ xfce4-power-manager &
 bluetoothctl power on &
 /usr/lib/gsd-xsettings &
 ~/scripts/set-screen.sh work &
-~/scripts/set-privoxy.sh on &
 ~/scripts/autostart_wait.sh &
