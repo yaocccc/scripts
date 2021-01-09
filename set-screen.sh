f3594eb6d993333d97ca641fe4d78c72869cf3f6
chenyc <flyforest@outlook.com>
2021-01-10 00:59:02 +0800

    设置分辨率的脚本添加 复制屏幕 配置

--- set-screen.sh
+++ set-screen.sh
@@ -5,22 +5,28 @@ DISCONNECT_SCREEN=$(xrandr | grep DisplayPort | grep ' disconnected ' | awk '{pr
 
 home() {
     if [ "$CONNECT_SCREEN" == "" ]; then one; return; fi
-    xrandr --output eDP --mode 1440x900 --pos 1920x325 --rotate normal --output $DISCONNECT_SCREEN --off --output $CONNECT_SCREEN --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP --mode 1440x900 --pos 1920x325 --output $DISCONNECT_SCREEN --off --output $CONNECT_SCREEN --primary --mode 1920x1080 --pos 0x0 
     ~/scripts/edit-profile.sh SCREEN_MODE HOME
 }
 work() {
     if [ "$CONNECT_SCREEN" == "" ]; then one; return; fi
-    xrandr --output eDP --mode 1440x900 --pos 0x525 --rotate normal --output $DISCONNECT_SCREEN --off --output $CONNECT_SCREEN --primary --mode 1920x1080 --pos 1440x0 --rotate normal
+    xrandr --output eDP --mode 1440x900 --pos 0x525 --output $DISCONNECT_SCREEN --off --output $CONNECT_SCREEN --primary --mode 1920x1080 --pos 1440x0 
     ~/scripts/edit-profile.sh SCREEN_MODE WORK
 }
+copy() {
+    if [ "$CONNECT_SCREEN" == "" ]; then one; return; fi
+    xrandr --output eDP --primary --mode 1440x900 --pos 0x0 --output $CONNECT_SCREEN --same-as eDP
+    ~/scripts/edit-profile.sh SCREEN_MODE ONE
+}
 one() {
-    xrandr --output eDP --primary --mode 1440x900 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
+    xrandr --output eDP --primary --mode 1440x900 --pos 0x0 --output DisplayPort-0 --off --output DisplayPort-1 --off
     ~/scripts/edit-profile.sh SCREEN_MODE ONE
 }
 
 case $1 in
     home) home ;;
     work) work ;;
+    copy) copy ;;
     one) one ;;
     *) 
         test "`ip addr show | ag '192.168.2'`" != "" && home
