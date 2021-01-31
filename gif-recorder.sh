cf224845b0f21c29283ff7ed24352150ebd27e02
chenyc <flyforest@outlook.com>
2021-01-31 22:45:14 +0800

    fix

--- gif-recorder.sh
+++ gif-recorder.sh
@@ -4,17 +4,6 @@ gif_file=~/show.gif
 let x y w h
 source ~/.profile
 
-while getopts "m:e:d:w:f:" opt
-do
-    case $opt in
-        m) mode=$OPTARG ;;
-        e) cmd=$OPTARG ;;
-        d) record_time=$OPTARG ;;
-        w) wait_time=$OPTARG ;;
-        f) gif_file=$OPTARG ;;
-    esac
-done
-
 case $SCREEN_MODE in
     HOME) S1_X=1920 S1_Y=325 S2_X=0 S2_Y=0 ;;
     WORK) S1_X=0 S1_Y=525 S2_X=1440 S2_Y=0 ;;
@@ -24,7 +13,7 @@ esac
 
 case $SCREEN_MODE in
     ONE)
-        case $mode in
+        case $1 in
             1) let x=$S1_X y=$S1_Y w=1440 h=900 ;;
             *)
                 echo 1: 内置屏幕-全屏
@@ -33,7 +22,7 @@ case $SCREEN_MODE in
         esac
         ;;
     *)
-        case $mode in
+        case $1 in
             1) let x=$S1_X     y=$S1_Y     w=1440     h=900 ;;
             2) let x=$S2_X     y=$S2_Y     w=1920     h=1080 ;;
             3) let x=$S2_X+478 y=$S2_Y+229 w=964      h=736 ;;
@@ -47,37 +36,11 @@ case $SCREEN_MODE in
                 ;;
         esac
         ;;
-    esac
+esac
 
-showTimer() {
-    i1=$1 i2=$2
-    arr=( "⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏" )
-    index=0
-
-    while [ $i1 -ge 1 ]
-    do
-        let index++
-        let index=index%10
-        printf "Starting record on %2d seconds after %s \r" "$i1" "${arr[$index]}"
-        let i1--
-        sleep 1
-    done
-    while [ $i2 -ge 1 ]
-    do
-        let index++
-        let index=index%10
-        let i2--
-        printf "Recording %2d %s \r" "$i2" "${arr[$index]}"
-        sleep 1
-    done
-}
-
-if [ "$cmd" == "" ]; then
-    showTimer ${wait_time-3} ${record_time-10} &
-else
-    wait_time=0
-    cmd="-e "$cmd
+if [ "$2" != "" ]; then
+    cmd="-e "$2
 fi
 
-byzanz-record -x $x -y $y -w $w -h $h --delay=${wait_time-3} -d ${record_time-10} -v $gif_file $cmd
+byzanz-record -x $x -y $y -w $w -h $h -v $gif_file $cmd
 sleep 1
