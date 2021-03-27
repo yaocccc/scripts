b984d5669cc851e7a09663cb58374294c2d6579d
chenyc <flyforest@outlook.com>
2021-03-27 16:46:21 +0800

    更新消息通知配置

--- wine-notify.sh
+++ wine-notify.sh
@@ -5,5 +5,5 @@ dbus-monitor "interface='org.freedesktop.DBus',member='RequestName'" | while rea
 do
     a=$(($a+1))
     a=$(($a%3))
-    test $a = 0 && notify-send "💬 $(date)" "有未读消息"
+    test $a = 0 && notify-send "💬 $(date)" -u low
 done
