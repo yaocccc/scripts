1def225f2b79a8850526274133ee91190e4891c8
chenyc <flyforest@outlook.com>
2021-03-30 11:51:30 +0800

    更新启动脚本

--- autostart.sh
+++ autostart.sh
@@ -2,14 +2,18 @@
 # DWM自启动脚本
 
 settings() {
+    [ $1 ] && sleep $1
+
     xset s 600
     syndaemon -i 1 -t -K -R -d
     xss-lock -- ~/scripts/app-starter.sh blurlock &
     ~/scripts/app-starter.sh off_privoxy &
-    sleep 1 && ~/scripts/set-screen.sh &
+    ~/scripts/set-screen.sh &
 }
 
 daemons() {
+    [ $1 ] && sleep $1
+
     fcitx &
     pactl info &
     nm-applet &
@@ -18,12 +22,12 @@ daemons() {
     /usr/lib/gsd-xsettings &
     ~/scripts/wine-notify.sh &
     dunst -conf ~/scripts/config/dunst.conf &
-
-    sleep 1
-    picom --config ~/scripts/config/compton.conf &
+    sleep 1 && picom --config ~/scripts/config/compton.conf &
 }
 
 every10s() {
+    [ $1 ] && sleep $1
+
     while true
     do
         ~/scripts/set-screen.sh check &
@@ -33,6 +37,8 @@ every10s() {
 }
 
 every1000s() {
+    [ $1 ] && sleep $1
+
     while true
     do
         xset -b
@@ -40,12 +46,13 @@ every1000s() {
         feh --randomize --bg-fill ~/Pictures/* &
         weather=`curl -sf 'wttr.in/ShangHai?format=1' | sed 's/ \|+//g'`
         [ ${#weather} -ge 10 ] && weather="🌈"
+        [ ${#weather} -le 4 ]  && weather="🌈"
         ~/scripts/edit-profile.sh weather "$weather"
         sleep 1000
     done
 }
 
-settings &
-daemons &
-every10s &
-every1000s &
+settings 1 &
+daemons 3 &
+every10s 5 &
+every1000s 30 &
