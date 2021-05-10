00c8c63a30dc55febdbdff6368763ca01805a3f1
chenyc <flyforest@outlook.com>
2021-05-10 16:49:53 +0800

    收取邮件逻辑变更

--- autostart.sh
+++ autostart.sh
@@ -47,9 +47,7 @@ every1000s() {
         [ ! $weather ] || [ ${#weather} -ge 20 ] && weather="🌈"
         ~/scripts/edit-profile.sh weather "$weather"
 
-        mail_count=`fetchmail -k | wc -l`
-        mail_count=$(($mail_count-1))
-        [ "$mail_count" -gt 0 ] && notify-send "💬有"$mail_count"条新邮件"
+        fetchmail -k &
 
         sleep 1000
 
