2ea963250007040a57cdc0768b0f0b4f7bbfa6b0
chenyc <flyforest@outlook.com>
2022-12-26 14:25:42 +0800

    update rofi.sh

--- rofi.sh
+++ rofi.sh
@@ -9,12 +9,13 @@
 source ~/.profile
 
 ##### MAIN_MENU ####
-    main_menu_items=('set wallpaper' 'update statusbar' 'toggle server' 'set backlight')
+    main_menu_items=('1. set wallpaper' '2. update statusbar' '3. toggle server' '4. set backlight' '5. power menu')
     main_menu_cmds=(
         'feh --randomize --bg-fill ~/Pictures/wallpaper/*.png; show_main_menu' # 执行完不退出脚本继续执行show_main_menu
         'coproc ($DWM/statusbar/statusbar.sh updateall > /dev/null 2>&1); show_main_menu'
-        'echo -en "\0new-selection\x1ftrue\n"; show_toggle_server_menu'  # 加前面的echo是设置进入二级菜单时将selection置为新
-        'echo -en "\0new-selection\x1ftrue\n"; show_set_backlight_menu'  # 加前面的echo是设置进入二级菜单时将selection置为新
+        'show_toggle_server_menu'
+        'show_set_backlight_menu'
+        'show_power_menu'
     )
 
 ##### TOGGLE_SERVER_MENU #####
@@ -37,8 +38,13 @@ source ~/.profile
     set_backlight_menu_cmds=('show_set_backlight_menu2 内置屏幕' 'show_set_backlight_menu2 外置屏幕')
     set_backlight_menu_items2=('1.0' '0.8' '0.6' '0.4' '0.2')
 
+##### POWER MENU #####
+    power_menu_items=('poweroff' 'reboot')
+    power_menu_cmds=('sudo poweroff' 'sudo reboot')
+
 ###### SHOW MENU ######
     show_main_menu() {
+        echo -en "\0new-selection\x1ftrue\n"
         echo -e "\0prompt\x1fmenu\n"
         echo -en "\0data\x1fMAIN_MENU\n"
         for item in "${main_menu_items[@]}"; do
@@ -46,6 +52,7 @@ source ~/.profile
         done
     }
     show_toggle_server_menu() {
+        echo -en "\0new-selection\x1ftrue\n"
         echo -e "\0prompt\x1ftoggle\n"
         echo -en "\0data\x1fTOGGLE_SERVER_MENU\n"
         for item in "${toggle_server_menu_items[@]}"; do
@@ -53,6 +60,7 @@ source ~/.profile
         done
     }
     show_set_backlight_menu() {
+        echo -en "\0new-selection\x1ftrue\n"
         echo -e "\0prompt\x1fselect\n"
         echo -en "\0data\x1fSET_BACKLIGHT_MENU\n"
         for item in "${set_backlight_menu_items[@]}"; do
@@ -60,9 +68,18 @@ source ~/.profile
         done
     }
     show_set_backlight_menu2() {
+        echo -en "\0new-selection\x1ftrue\n"
         echo -e "\0prompt\x1flight\n"
         echo -en "\0data\x1fSET_BACKLIGHT_$1\n"
         for item in "${set_backlight_menu_items2[@]}"; do
+            echo "$item"
+        done
+    }
+    show_power_menu() {
+        echo -en "\0new-selection\x1ftrue\n"
+        echo -e "\0prompt\x1fpower\n"
+        echo -en "\0data\x1fPOWER_MENU\n"
+        for item in "${power_menu_items[@]}"; do
             echo "$item"
         done
     }
@@ -95,6 +112,10 @@ source ~/.profile
                 out=$(xrandr | grep -v eDP-1 | grep -w 'connected' | awk '{print $1}'); light=$1
                 xrandr --output $out --brightness $light
             ;;
+            POWER_MENU)
+                for i in "${!power_menu_items[@]}"; do
+                    [ "$*" = "${power_menu_items[$i]}" ] && eval "${power_menu_cmds[$i]}"
+                done
         esac
     }
 
