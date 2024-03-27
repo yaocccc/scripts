9692023ab05a2625ce6f6e7166b9430cf23ffc43
CH <ch915753811@gmail.com>
2024-03-27 21:24:21 +0800

    修改了picom配置文件和data.sh

--- set_screen.sh
+++ set_screen.sh
@@ -1,58 +1,67 @@
 #! /bin/bash
-:<<!
+: <<!
   设置屏幕分辨率的脚本(xrandr命令的封装)
   one: 只展示一个内置屏幕 2560x1600 缩放为 1440x900
   two: 左边展示外接屏幕 - 右边展示内置屏幕 都用匹配1080p屏幕DPI的缩放比
   check: 检测显示器连接状态是否变化 变化则自动调整输出情况
 !
 
-# 定义内置屏幕接口和外接屏幕接口
 INNER_PORT=eDP-1
-OUTPORT1=DP-1
-OUTPORT2=DP-2
+MODE=TB
+OUT_PORT=HDMI-1
+[ "$(xrandr | grep '2560x1440')" ] && MODE=H$MODE
 
-MODE=LR # LR 左右 TB 上下
-[ "$(xrandr | grep '3440x1440')" ] && MODE=HLR
+__setbg() {
+	feh --randomize --bg-fill ~/Pictures/wallpaper/*.png
+}
+
+__get_inner_view() {
+	case "$MODE" in
+	ONE) echo "--output $INNER_PORT --mode 2560x1600  --pos 0x0" ;;
+	LR) echo "--output $INNER_PORT --mode 2880x1800  --pos 1920x320" ;;
+	TB) echo "--output $INNER_PORT --mode 2560x1600 --pos 0x1440 --rotate normal" ;;
+	HLR) echo "--output $INNER_PORT --mode 2880x1800 --pos 2560x320" ;;
+	HTB) echo "--output $INNER_PORT --mode 2880x1800 --pos 500x1080" ;;
+	esac
+}
+
+__get_outer_view() {
+	outport=$1
+	case "$MODE" in
+	LR) echo "--output $OUT_PORT --mode 1920x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+	TB) echo "--output $OUT_PORT --primary --mode 2560x1440 --pos 0x0 " ;;
+	HLR) echo "--output $OUT_PORT --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+	HTB) echo "--output $OUT_PORT --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+	esac
+}
+
+__get_off_views() {
+	for sc in $(xrandr | grep 'connected' | awk '{print $1}'); do echo "--output $sc --off "; done
+}
+
+one() {
+	MODE=ONE
+	xrandr $(__get_off_views) $(__get_inner_view)
+	__setbg
+}
 
 two() {
-    # 查找已连接、未连接的外接接口
-    OUTPORT_CONNECTED=$(xrandr | grep -v $INNER_PORT | grep -w 'connected' | awk '{print $1}')
-    OUTPORT_DISCONNECTED=$(xrandr | grep -v $INNER_PORT | grep -w 'disconnected' | awk '{print $1}')
-    [ ! "$OUTPORT_CONNECTED" ] && one && return # 如果没有外接屏幕则直接调用one函数
+	OUTPORT_CONNECTED=$(xrandr | grep -v $INNER_PORT | grep -w 'connected' | awk '{print $1}')
+	[ ! "$OUTPORT_CONNECTED" ] && one && return
 
-    [ $MODE = "LR" ] && \
-        xrandr --output $INNER_PORT --mode 2560x1600 --pos 1920x320 --scale 1x1 \
-               --output $OUTPORT_CONNECTED --mode 2560x1440 --pos 0x0 --scale 1x1 --primary \
-               --output $OUTPORT_DISCONNECTED --off
-
-    [ $MODE = "HLR" ] && \
-        xrandr --output $INNER_PORT --mode 2560x1600 --pos 2560x320 --scale 1x1 \
-               --output $OUTPORT_CONNECTED --mode 2560x1440 --pos 0x0 --scale 1x1 --primary \
-               --output $OUTPORT_DISCONNECTED --off
-
-    [ $MODE = "TB" ] && \
-        xrandr --output $INNER_PORT --mode 2560x1600 --pos 500x1080 --scale 1x1 \
-               --output $OUTPORT_CONNECTED --mode 2560x1440 --pos 0x0 --scale 1x1 --primary \
-               --output $OUTPORT_DISCONNECTED --off
-
-    feh --randomize --bg-fill ~/Pictures/wallpaper/*.{png,jpg}
+	xrandr $(__get_off_views) $(__get_inner_view) $(__get_outer_view $OUTPORT_CONNECTED)
+	__setbg
 }
-one() {
-    xrandr --output $INNER_PORT --mode 2560x1600 --pos 0x0 --scale 1x1 --primary \
-           --output $OUTPORT1 --off \
-           --output $OUTPORT2 --off
-    feh --randomize --bg-fill ~/Pictures/wallpaper/*.{png,jpg}
-}
+
 check() {
-    CONNECTED_PORTS=$(xrandr | grep -w 'connected' | awk '{print $1}' | wc -l)
-    CONNECTED_MONITORS=$(xrandr --listmonitors | sed 1d | awk '{print $4}' | wc -l)
-    [ $CONNECTED_PORTS -gt $CONNECTED_MONITORS ] && two # 如果当前连接接口多于当前输出屏幕 则调用two
-    [ $CONNECTED_PORTS -lt $CONNECTED_MONITORS ] && one # 如果当前连接接口少于当前输出屏幕 则调用one
+	CONNECTED_PORTS=$(xrandr | grep -w 'connected' | awk '{print $1}' | wc -l)
+	CONNECTED_MONITORS=$(xrandr --listmonitors | sed 1d | awk '{print $4}' | wc -l)
+	[ $CONNECTED_PORTS -gt $CONNECTED_MONITORS ] && two
+	[ $CONNECTED_PORTS -lt $CONNECTED_MONITORS ] && one
 }
 
 case $1 in
-    one) one ;;
-    two) two ;;
-    check) check ;;
-    *) check ;;
+one) one ;;
+two) two ;;
+*) check ;;
 esac
