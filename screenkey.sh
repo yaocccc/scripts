0ab64c7056c74fd28943e7933eee0c29f1506bdf
chenyc <flyforest@outlook.com>
2024-10-22 12:26:36 +0800

    update

--- screenkey.sh
+++ screenkey.sh
@@ -4,4 +4,5 @@ CONNECTED_MONITORS=$(xrandr --listmonitors | sed 1d | awk '{print $4}' | wc -l)
 case $CONNECTED_MONITORS in
     1) killall screenkey || screenkey -p fixed -g 66%x8%+17%-5% & ;;
     2) killall screenkey || screenkey -p fixed -g 50%x8%+25%-11% & ;;
+    3) killall screenkey || screenkey & ;;
 esac
--- set_screen.sh
+++ set_screen.sh
@@ -7,8 +7,10 @@
 !
 
 INNER_PORT=eDP-1
-MODE=LR
-[ "$(xrandr | grep '3440x1440')" ] && MODE=H$MODE
+_MODE=THREE
+MODE=$_MODE
+# [ "$(xrandr | grep '3840x2160')" ] && MODE=H$_MODE
+# [ "$(xrandr | grep '3440x1440')" ] && MODE=L$_MODE
 
 __setbg() {
     feh --randomize --bg-fill ~/Pictures/wallpaper/*.png
@@ -19,18 +21,25 @@ __get_inner_view() {
         ONE) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 0x0" ;;
         LR) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 1920x320" ;;
         TB) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 500x1080" ;;
-        HLR) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 2560x320" ;;
+        HLR) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 1920x320" ;;
         HTB) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 500x1080" ;;
+        LLR) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 2560x320" ;;
+        LTB) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 500x1080" ;;
+        THREE) echo "--output $INNER_PORT --mode 2880x1800 --scale 0.5x0.5 --pos 3640x880" ;;
     esac
 }
 
 __get_outer_view() {
     outport=$1
+    outport2=$2
     case "$MODE" in
         LR) echo "--output $outport --mode 1920x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
         TB) echo "--output $outport --mode 1920x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
-        HLR) echo "--output $outport --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
-        HTB) echo "--output $outport --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+        HLR) echo "--output $outport --mode 3840x2160 --scale 0.5x0.5 --pos 0x0 --primary" ;;
+        HTB) echo "--output $outport --mode 3840x2160 --scale 0.5x0.5 --pos 0x0 --primary" ;;
+        LLR) echo "--output $outport --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+        LTB) echo "--output $outport --mode 2560x1080 --scale 0.9999x0.9999 --pos 0x0 --primary" ;;
+        THREE) echo "--output $outport --mode 1920x1080 --scale 0.9999x0.9999 --rotate left --pos 0x0 --output $outport2 --mode 2560x1080 --scale 0.9999x0.9999 --pos 1080x300 --primary" ;;
     esac
 }
 
@@ -52,15 +61,28 @@ two() {
     __setbg
 }
 
+three() {
+    HDMI_OUTPUT=$(xrandr | grep -v $INNER_PORT | grep -w 'connected' | grep HDMI | awk '{print $1}')
+    DP_OUTPUT=$(xrandr | grep -v $INNER_PORT | grep -w 'connected' | grep DP | awk '{print $1}')
+
+    [ ! "$HDMI_OUTPUT" ] && one && return
+
+    xrandr $(__get_off_views) $(__get_inner_view) $(__get_outer_view $DP_OUTPUT $HDMI_OUTPUT)
+    __setbg
+}
+
 check() {
     CONNECTED_PORTS=$(xrandr | grep -w 'connected' | awk '{print $1}' | wc -l)
     CONNECTED_MONITORS=$(xrandr --listmonitors | sed 1d | awk '{print $4}' | wc -l)
-    [ $CONNECTED_PORTS -gt $CONNECTED_MONITORS ] && two
-    [ $CONNECTED_PORTS -lt $CONNECTED_MONITORS ] && one
+    [ $CONNECTED_PORTS -eq $CONNECTED_MONITORS ] && return
+    [ $CONNECTED_PORTS -eq 3 ] && three
+    [ $CONNECTED_PORTS -eq 2 ] && two
+    [ $CONNECTED_PORTS -eq 1 ] && one
 }
 
 case $1 in
     one) one ;;
     two) two ;;
+    three) three ;;
     *) check ;;
 esac
